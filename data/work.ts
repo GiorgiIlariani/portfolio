0ca80242cbfe7fed1318ebe91a55bf0e0b386e7b
GiorgiIlariani <lmaisuradze3333@gmail.com>
2023-10-16 15:36:54 +0400

    adds threads app

--- data/work.ts
+++ data/work.ts
@@ -11,59 +11,67 @@ import carhub from "@/public/assets//images/carhub.png";
 import youtube_clone from "@/public/assets/images/youtube_clone.png";
 import GymExercises from "@/public/assets/images/gym_exercises.png";
 import MyMarket from "@/public/assets/images/mymarket.png";
+import ThreadsApp from '@/public/assets/images/threads.png'
 
 export const data = [
-  {
+    {
     id: 1,
+    name: "Threads App",
+    image: ThreadsApp,
+    github: "https://github.com/GiorgiIlariani/threads",
+    live: "https://threads-kappa-three.vercel.app/",
+  },
+  {
+    id: 2,
     name: "myMarket",
     image: MyMarket,
     github: "https://github.com/GiorgiIlariani/mymarket",
     live: "https://mymarket-iota.vercel.app/",
   },
   {
-    id: 2,
+    id: 3,
     name: "gym_exercises",
     image: GymExercises,
     github: "https://github.com/GiorgiIlariani/gym_exercises",
     live: "https://gym-exercises-self.vercel.app/",
   },
   {
-    id: 3,
+    id: 4,
     name: "carhub",
     image: carhub,
     github: "https://github.com/GiorgiIlariani/carhub",
     live: "https://carhub-alpha.vercel.app/",
   },
   {
-    id: 4,
+    id: 5,
     name: "youtube_clone",
     image: youtube_clone,
     github: "https://github.com/GiorgiIlariani/Youtube_clone",
     live: "https://youtube-clone-psi-sandy.vercel.app/",
   },
   {
-    id: 5,
+    id: 6,
     name: "Cv_Creator",
     image: Cv_Creator,
     github: "https://github.com/GiorgiIlariani/Cv_Creator",
     live: "https://cv-creator-xi.vercel.app/",
   },
   {
-    id: 6,
+    id: 7,
     name: "Covid_Questionary",
     image: Covid_Questionary,
     github: "https://github.com/GiorgiIlariani/Covid_Questionary",
     live: "https://covid-questionary.vercel.app/",
   },
   {
-    id: 7,
+    id: 8,
     name: "E-Commerce",
     image: E_commerce,
     github: "https://github.com/GiorgiIlariani/E-commerce",
     live: "https://e-commerce-pi-eight.vercel.app/",
   },
   {
-    id: 8,
+    id: 9,
     name: "Weather",
     image: Weather_api,
     github: "https://github.com/GiorgiIlariani/weather_api",
