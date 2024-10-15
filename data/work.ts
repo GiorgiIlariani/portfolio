16d11350337d70db4cabf1b06df9c9f60881c640
GiorgiIlariani <lmaisuradze3333@gmail.com>
2024-10-15 19:13:05 +0400

    adds project

--- data/work.ts
+++ data/work.ts
@@ -2,83 +2,59 @@ import Covid_Questionary from "@/public/assets/images/Covid_questionary.png";
 import ThreadsApp from '@/public/assets/images/threads.png'
 import Aragveli from '@/public/assets/images/aragveli.png';
 import Blogs from '@/public/assets/images/blogs.png';
-import Evently from '@/public/assets/images/evently.png'
-import MyMarket from '@/public/assets/images/mymarket.png'
+import Evently from '@/public/assets/images/evently.png';
+import MyMarket from '@/public/assets/images/mymarket.png';
+import RealEstateManager from '@/public/assets/images/real-estate-manager.png';
 
 
 export const data = [
   {
     id: 1,
+    name: "Real Estate Manager",
+    image: RealEstateManager,
+    github: "https://github.com/GiorgiIlariani/real-estate-manager",
+    live: "https://real-estate-manager-one.vercel.app/",
+  },
+  {
+    id: 2,
     name: "E-commerce Web Site",
     image: MyMarket,
     github: "https://github.com/GiorgiIlariani/e-commerce",
     live: "https://mymarket-delta.vercel.app/",
   },
   {
-    id: 2,
+    id: 3,
     name: "300 Aragveli's page",
     image: Aragveli,
     github: "https://github.com/GiorgiIlariani/300_aragvelii",
     live: "https://300-aragvelii.vercel.app/",
   },
   {
-    id: 3,
+    id: 4,
     name: "Evently",
     image: Evently,
     github: "https://github.com/GiorgiIlariani/evently",
     live: "https://evently-eight-pi.vercel.app/",
   },
   {
-    id: 4,
+    id: 5,
     name: "Threads App",
     image: ThreadsApp,
     github: "https://github.com/GiorgiIlariani/threads",
     live: "https://threads-kappa-three.vercel.app/",
   },
   {
-    id: 5,
+    id: 6,
     name: "Blogs App",
     image: Blogs,
     github: "https://github.com/GiorgiIlariani/blogs",
     live: "https://blogs-five-sigma.vercel.app/",
   },
   {
-    id: 6,
+    id: 7,
     name: "Covid_Questionary",
     image: Covid_Questionary,
     github: "https://github.com/GiorgiIlariani/Covid_Questionary",
     live: "https://covid-questionary.vercel.app/",
   },
 ];
-
-
-
-
- // {
-  //   id: 5,
-  //   name: "youtube_clone",
-  //   image: Youtube_clone,
-  //   github: "https://github.com/GiorgiIlariani/Youtube_clone",
-  //   live: "https://youtube-clone-psi-sandy.vercel.app/",
-  // },
-  // {
-  //   id: 6,
-  //   name: "Pricewise",
-  //   image: Pricewise,
-  //   github: "https://github.com/GiorgiIlariani/pricewise",
-  //   live: "https://pricewise-alpha-eight.vercel.app/",
-  // },
-  // {
-  //   id: 7,
-  //   name: "gym_exercises",
-  //   image: GymExercises,
-  //   github: "https://github.com/GiorgiIlariani/gym_exercises",
-  //   live: "https://gym-exercises-self.vercel.app/",
-  // },
-  // {
-  //   id: 8,
-  //   name: "carhub",
-  //   image: Carhub,
-  //   github: "https://github.com/GiorgiIlariani/carhub",
-  //   live: "https://carhub-alpha.vercel.app/",
-  // },
